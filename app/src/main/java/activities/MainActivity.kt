3e131783ee0b50bbec5450e7d60ec69f616cce82
Evghenii Smirnov <evghenii.smirnov@feel-it-services.com>
2021-01-14 08:35:08 +0200

    navigation

--- app/src/main/java/activities/MainActivity.kt
+++ app/src/main/java/activities/MainActivity.kt
@@ -3,6 +3,7 @@ package activities
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
 import com.popularmovies.R
 
@@ -10,7 +11,9 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val navController = findNavController(R.id.navHostFragment)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
         NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
